6a531409c2ebd298b1c2a94d889e57c47324cc2a
Maxime <leu.maxime27@gmail.com>
2025-10-13 16:07:57 +0200

    Ajout de tous les controllers .

---
+++ src/Form/CoursType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cours;
+use App\Entity\Jour;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CoursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('ageMini')
+            ->add('heureDebut')
+            ->add('heureFin')
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'id',
+            ])
+            ->add('jour', EntityType::class, [
+                'class' => Jour::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cours::class,
+        ]);
+    }
+}
---
+++ src/Form/InscriptionType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cours;
+use App\Entity\Eleve;
+use App\Entity\Inscription;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateInscrption')
+            ->add('eleve', EntityType::class, [
+                'class' => Eleve::class,
+                'choice_label' => 'id',
+            ])
+            ->add('cours', EntityType::class, [
+                'class' => Cours::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Inscription::class,
+        ]);
+    }
+}
