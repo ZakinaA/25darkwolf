932e5b3eef38d369cc5151f182849153390241f2
Maxime <leu.maxime27@gmail.com>
2025-10-13 15:44:39 +0200

    Ajout de l'entity Cours.

---
+++ src/Entity/Cours.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoursRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoursRepository::class)]
+class Cours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $ageMini = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTime $heureDebut = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTime $heureFin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(?string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getAgeMini(): ?int
+    {
+        return $this->ageMini;
+    }
+
+    public function setAgeMini(?int $ageMini): static
+    {
+        $this->ageMini = $ageMini;
+
+        return $this;
+    }
+
+    public function getHeureDebut(): ?\DateTime
+    {
+        return $this->heureDebut;
+    }
+
+    public function setHeureDebut(?\DateTime $heureDebut): static
+    {
+        $this->heureDebut = $heureDebut;
+
+        return $this;
+    }
+
+    public function getHeureFin(): ?\DateTime
+    {
+        return $this->heureFin;
+    }
+
+    public function setHeureFin(?\DateTime $heureFin): static
+    {
+        $this->heureFin = $heureFin;
+
+        return $this;
+    }
+}
