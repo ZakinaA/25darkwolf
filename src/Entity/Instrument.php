852cf3b4a892d5ce97f7850019d0de8e28e6b297
sio2 <antoninfouchet@gmail.com>
2025-10-13 14:45:47 +0200

    Entities Instrument

---
+++ src/Entity/Instrument.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InstrumentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InstrumentRepository::class)]
+class Instrument
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $numSerie = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $dateAchat = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $prixAchat = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $utilisation = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cheminImage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumSerie(): ?string
+    {
+        return $this->numSerie;
+    }
+
+    public function setNumSerie(?string $numSerie): static
+    {
+        $this->numSerie = $numSerie;
+
+        return $this;
+    }
+
+    public function getDateAchat(): ?\DateTime
+    {
+        return $this->dateAchat;
+    }
+
+    public function setDateAchat(?\DateTime $dateAchat): static
+    {
+        $this->dateAchat = $dateAchat;
+
+        return $this;
+    }
+
+    public function getPrixAchat(): ?float
+    {
+        return $this->prixAchat;
+    }
+
+    public function setPrixAchat(?float $prixAchat): static
+    {
+        $this->prixAchat = $prixAchat;
+
+        return $this;
+    }
+
+    public function getUtilisation(): ?string
+    {
+        return $this->utilisation;
+    }
+
+    public function setUtilisation(?string $utilisation): static
+    {
+        $this->utilisation = $utilisation;
+
+        return $this;
+    }
+
+    public function getCheminImage(): ?string
+    {
+        return $this->cheminImage;
+    }
+
+    public function setCheminImage(?string $cheminImage): static
+    {
+        $this->cheminImage = $cheminImage;
+
+        return $this;
+    }
+}
