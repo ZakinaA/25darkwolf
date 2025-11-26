8cc365fcb71e878ad49af103c489a1fc3e09b499
Come <aussant.come@gmail.com>
2025-11-26 09:40:43 +0100

    Modification de la page d'accueil pour ajouter le planning

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CoursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,11 +13,16 @@ class HomeController extends AbstractController
      * C'est cette ligne qui crée la route 'app_home'
      */
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(): Response
+    public function index(CoursRepository $coursRepository): Response
     {
-        // Ceci est votre page d'accueil publique
+        // 1. Récupérer la liste des cours triés en utilisant la méthode du repository.
+        // Cela suppose que la méthode findAllSortedByDayAndTime() existe dans votre CoursRepository.
+        $planningCours = $coursRepository->findAllSortedByDayAndTime();
+
+        // 2. Ceci est votre page d'accueil publique
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'planning_cours' => $planningCours, // Transmission des données pour l'affichage du planning
         ]);
     }
 }
